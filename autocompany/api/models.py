1a4a8abd9e0a2fc72235bbf306a4913a5bd63e0e
Mansoor M. Sathir <sathir@live.com>
2022-05-28 15:07:31 +0530

    Modified client model to show customer name also in rest view

--- autocompany/api/models.py
+++ autocompany/api/models.py
@@ -25,7 +25,7 @@ class Client(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.customer_code
+        return self.customer_code +" "+ self.pref_name
 
 
 class ShoppingCartItem(models.Model):
