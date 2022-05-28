4e21f308416b6f68de40dbe73c2dae662934620c
Mansoor M. Sathir <sathir@live.com>
2022-05-28 15:00:00 +0530

    Modified product model to show item code in rest view

--- autocompany/api/models.py
+++ autocompany/api/models.py
@@ -8,6 +8,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.item_code
+
 
 class Client(models.Model):
     GENDER = [('M', 'Male'), ('F', 'Female')]
